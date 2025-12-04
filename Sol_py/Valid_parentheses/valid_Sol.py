28963e7dfe736109bf679c39e423b0771b804ac2
KPIv77 <kitisakpholor@gmail.com>
2025-12-04 19:36:35 +0700

    check by if elif

--- Sol_py/Valid_parentheses/valid_Sol.py
+++ Sol_py/Valid_parentheses/valid_Sol.py
@@ -12,7 +12,16 @@ class Solution:
                 return True
             elif s[index_s] == "{" and s[-(index_s+1)] == "}":
                 return True 
-            
+            elif s[index_s] == "(" and s[-(index_s+1)] != ")":
+                return False
+            elif s[index_s] == "[" and s[-(index_s+1)] != "]":
+                return False
+            elif s[index_s] == "{" and s[-(index_s+1)] != "}":
+                return False
+
+
+
            
+          
 s = Solution()
 print(s.isValid("((((()))))"))                          
