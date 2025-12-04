31d3009676ca1130c69fc8b726baa73869911e89
KPIv77 <kitisakpholor@gmail.com>
2025-12-04 12:45:08 +0700

    Sol_2_valid

--- Sol_py/Valid_parentheses/valid_Sol.py
+++ Sol_py/Valid_parentheses/valid_Sol.py
@@ -1,33 +1,18 @@
 
-#brackets_1 = ()
-#brackets_2 = []
-#brackets_3 = {}
-
-#Try use git branch
-
 
 
 class Solution:
     def isValid(self, s:str) -> bool:
         
-        brackets_1 = []
-        brackets_2 = []
-        brackets_3 = []
         
-        for item in s:
-            if item == "(" or item == ")":
-                brackets_1.append(item)
+        for index_s in range(len(s)):
+            if s[index_s] == "(" and s[-(index_s+1)] == ")":
+                return True 
+            elif s[index_s] == "[" and s[-(index_s+1)] == "]":
+                return True
+            elif s[index_s] == "{" and s[-(index_s+1)] == "}":
+                return True 
             
-            elif item == "[" or item == "]":
-                brackets_2.append(item)
-                
-            elif item == "{" or item == "}": 
-                brackets_3.append(item)
-        
-        if len(brackets_1) % 2 == 0 and len(brackets_2) % 2 == 0 and len(brackets_3) % 2 == 0:
-            return True
-        else:
-            return False
-
-
-                
+           
+s = Solution()
+print(s.isValid("((((()))))"))                          
