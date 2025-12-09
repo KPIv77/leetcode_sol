755825526368364c5b63057aacf5f85d03a56b3e
KPIv77 <kitisakpholor@gmail.com>
2025-12-10 02:43:04 +0700

    test

--- Sol_py/Valid_parentheses/valid_Sol.py
+++ Sol_py/Valid_parentheses/valid_Sol.py
@@ -1,29 +1,23 @@
-
-"""
-step1 check even or odd  
-
-
-"""
-
-
-
 class Solution:
     def isValid(self, s:str) -> bool:
-       
+        
+        # Check odd or even
         if len(s) % 2 != 0:
             return False
-       
+        
+        # loop for check index
         for i in range(len(s) // 2):
             left = s[i]
             right = s[-(i + 1)]
            
+        # condition for check 
             if left == "(" and right != ")":
                 return False
             elif left == "[" and right != "]":
                 return False
             elif left == "{" and right != "}":
                 return False
-            elif left not in "([{":
+            else:
                 return False
            
         return True
